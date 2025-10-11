f2e58196dc34dd2c11cf3eeb9859256bbc6c0e79
Your Name <you@example.com>
2025-10-10 21:34:02 -0300

    testando outra forma de importar: usando FROM

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import calculo
+# import calculo
+# importando de outro jeito:
+from calculo import dobro, triplo
 
 print("ola mundo agora no vscode \n")
 print("programa dos calculos")
@@ -6,13 +8,13 @@ print("-" * 50)
 
 x = input("Digite o numero e vou dizer o dobro:")
 
-dobro = calculo.dobro(x)
+dobro = dobro(x)
 
 print(f"o dobro de {x} é {dobro}")
 
 
 y = input("Agora, digite outro numero e vou dizer o triplo")
 
-triplo = calculo.triplo(y)
+triplo = triplo(y)
 
 print(f"O trplo de {y} é {triplo}")
